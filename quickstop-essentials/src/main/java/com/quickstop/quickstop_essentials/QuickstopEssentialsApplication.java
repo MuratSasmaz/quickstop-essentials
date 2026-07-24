4ecd2cac7738d2a6828394737202c56b3f100a37
Amber Forrest <amber4est@gmail.com>
2026-07-24 04:03:54 -0400

    Initialize Spring Boot project for Deliverable 1

---
+++ quickstop-essentials/src/main/java/com/quickstop/quickstop_essentials/QuickstopEssentialsApplication.java
@@ -0,0 +1,13 @@
+package com.quickstop.quickstop_essentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickstopEssentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickstopEssentialsApplication.class, args);
+	}
+
+}
